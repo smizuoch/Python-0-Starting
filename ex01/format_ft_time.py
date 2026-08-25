065513dd1ecb3de7af9d7b88f7aef49c0a89a834
smizuoch <shotaro.mizuochi@proton.me>
2026-08-25 15:14:27 +0900

    Fix formatting and ensure consistent indentation in Hello.py and format_ft_time.py

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -5,7 +5,7 @@ seconds = time.time()
 current_date = datetime.datetime.now()
 
 print(
-	f"Seconds since January 1, 1970: {seconds:,.4f} "
-	f"or {seconds:.2e} in scientific notation"
+    f"Seconds since January 1, 1970: {seconds:,.4f} "
+    f"or {seconds:.2e} in scientific notation"
 )
 print(current_date.strftime("%b %d %Y"))
